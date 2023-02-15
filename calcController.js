2694f79d67975947fb23531a34299e22071a4743
Pedro Imbriani <pedro.imbriani.clear@gmail.com>
2023-02-15 11:23:41 -0300

    alteração nos eventos dos cursor

--- calcController.js
+++ calcController.js
@@ -15,16 +15,25 @@ class CalcController{
     setInterval(()=>{
         this.steDisplaayDateTime();
     },1000);
+    
+    }
+    addEventListenerAll(element, events, fn,){
+        events.split(" ").forEach(event => {
+                element.addEventListener(event, fn, false );
+
+        });
 
     }
     initButtonsEventes(){
       let buttons = document.querySelectorAll("#buttons > g, #partes > g");
       buttons.forEach((btn, index)=>{
         
-        btn.addEventListener('click', e =>{
+        this.addEventListenerAll(btn, 'click drag', e =>{
             console.log(btn.className.baseVal.replace("btn-",""));
-        })
-
+        });
+        this.addEventListenerAll(btn, "mouseover mouseup mousedown", e => {
+            btn.style.cursor = "pointer";
+        });
       })
 
       ;
