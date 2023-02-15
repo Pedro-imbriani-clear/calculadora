b8fc6c0d9df5f6022cb5a0c5a7c7b3634bb13a51
Pedro Imbriani <pedro.imbriani.clear@gmail.com>
2023-02-15 09:57:49 -0300

    configurando data e hora

--- controller/calcController.js
+++ controller/calcController.js
@@ -1,6 +1,7 @@
 class CalcController{
 
     constructor(){
+        this._locale= "pt-BR"
         this._displayCalcEl =  document.querySelector("#display"); 
         this._dateEl = document.querySelector("#data");
         this._timeEl =  document.querySelector("#hora");
@@ -8,25 +9,50 @@ class CalcController{
         this.initiazile();
     }
     initiazile(){
+        this.steDisplaayDateTime();
 
-    
-        this._dateEl.innerHTML = "10/04/2022";
-        this._timeEl.innerHTML = "21:30";
+    setInterval(()=>{
+        this.steDisplaayDateTime();
+    },1000);
 
     }
+    steDisplaayDateTime(){
+        this.displayDate = this.currentDate.toLocaleDateString(this._locale);  
+        this.displayTime = this.currentDate.toLocaleTimeString(this._locale); 
+    }
+    get displayTime(){
+
+        return  this._timeEl.innerHTML;
+
+    }
+    set displayTime(value){
+
+        return  this._timeEl.innerHTML = value;
+    }
+
+    get displayDate(){
+
+        return this._dateEl.innerHTML;
+
+    }
+    set displayDate(value){
+
+        return  this._dateEl.innerHTML = value;
+    }
+
     get displayCalc(){
 
         return this._displayCalcEl.innerHTML;
     }
-    set displayCalc(valor){
-        this._displayCalcEl.innerHTML = valor;
+    set displayCalc(value){
+        this._displayCalcEl.innerHTML = value;
     }
     get currentDate(){
-        return this._currentDate;
+        return new Date;
 
     }
-    set currentDate(valor){
+    set currentDate(value){
 
-    this._currentDate = valor
+    this._currentDate = value
     }
 }
