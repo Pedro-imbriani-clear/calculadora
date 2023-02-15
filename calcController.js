3f8b4f6f92e44142030ce13160df206220e81f91
Pedro Imbriani <pedro.imbriani.clear@gmail.com>
2023-02-15 09:57:49 -0300

    configurando data e hora

---
+++ calcController.js
@@ -0,0 +1,58 @@
+class CalcController{
+
+    constructor(){
+        this._locale= "pt-BR"
+        this._displayCalcEl =  document.querySelector("#display"); 
+        this._dateEl = document.querySelector("#data");
+        this._timeEl =  document.querySelector("#hora");
+        this._currentDate;
+        this.initiazile();
+    }
+    initiazile(){
+        this.steDisplaayDateTime();
+
+    setInterval(()=>{
+        this.steDisplaayDateTime();
+    },1000);
+
+    }
+    steDisplaayDateTime(){
+        this.displayDate = this.currentDate.toLocaleDateString(this._locale);  
+        this.displayTime = this.currentDate.toLocaleTimeString(this._locale); 
+    }
+    get displayTime(){
+
+        return  this._timeEl.innerHTML;
+
+    }
+    set displayTime(value){
+
+        return  this._timeEl.innerHTML = value;
+    }
+
+    get displayDate(){
+
+        return this._dateEl.innerHTML;
+
+    }
+    set displayDate(value){
+
+        return  this._dateEl.innerHTML = value;
+    }
+
+    get displayCalc(){
+
+        return this._displayCalcEl.innerHTML;
+    }
+    set displayCalc(value){
+        this._displayCalcEl.innerHTML = value;
+    }
+    get currentDate(){
+        return new Date;
+
+    }
+    set currentDate(value){
+
+    this._currentDate = value
+    }
+}
